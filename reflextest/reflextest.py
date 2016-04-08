b1eb3022fa31135ddf006d1f178a5bb8f0784e8a
Joshua Leon <joshualeon@Alveyworld-02.local>
2016-04-08 08:34:44 -0600

    Reflexes

---
+++ reflextest/reflextest.py
@@ -0,0 +1,61 @@
+#
+# Reflex Test on a 3x3 board
+#
+
+import pygame
+import math
+import game_mouse
+import random
+import datetime
+
+
+BACKGROUND_COLOR        = (200, 200, 200)
+BUTTON_OUTLINE_COLOR    = (  0,   0,   0)
+BUTTON_FILL_COLOR       = (255, 51, 153)
+BUG_COLOR       		= (86,195,86)
+
+class GameBoard(game_mouse.Game):
+    def __init__(self, width, height):
+        pass
+        
+    def game_logic(self, keys, newkeys, buttons, newbuttons, mouse_position):
+        pass
+    
+    def paint(self, surface):
+        pass
+        
+def processClick(board, click_x, click_y, bug, bugpos, width, height,timer):
+    pass
+
+def drawBug(surface, width, height, square, board):
+	pass
+	
+def drawTest(surface, spot):
+	pass
+
+def drawWindow(surface, width, height, board):
+    pass
+
+def clearBackground(surface, width, height):
+    pass
+
+def main():
+    pass
+    
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
